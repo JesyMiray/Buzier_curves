ca222ff8dbf5075a3191cca874f68ee09b6eb06b
jesuiie <aleksandrow.dima8@gmail.com>
2024-05-20 20:18:47 +0300

    an application for drawing Bezier curves

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Bezier_curves
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.addModeButton = new System.Windows.Forms.Button();
+            this.editButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(1013, 666);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox_Paint);
+            this.pictureBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseClick);
+            this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            this.pictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseUp);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(1086, 605);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(90, 41);
+            this.clearButton.TabIndex = 1;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(1042, 55);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(172, 259);
+            this.checkedListBox1.TabIndex = 2;
+            // 
+            // addModeButton
+            // 
+            this.addModeButton.Location = new System.Drawing.Point(1086, 429);
+            this.addModeButton.Name = "addModeButton";
+            this.addModeButton.Size = new System.Drawing.Size(90, 37);
+            this.addModeButton.TabIndex = 3;
+            this.addModeButton.Text = "add";
+            this.addModeButton.UseVisualStyleBackColor = true;
+            this.addModeButton.Click += new System.EventHandler(this.addModeButton_Click);
+            // 
+            // editButton
+            // 
+            this.editButton.Location = new System.Drawing.Point(1086, 488);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(90, 37);
+            this.editButton.TabIndex = 4;
+            this.editButton.Text = "edit";
+            this.editButton.UseVisualStyleBackColor = true;
+            this.editButton.Click += new System.EventHandler(this.editButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(1086, 551);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(90, 37);
+            this.deleteButton.TabIndex = 5;
+            this.deleteButton.Text = "delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(1226, 690);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.addModeButton);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.pictureBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Buzier Curves";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button addModeButton;
+        private System.Windows.Forms.Button editButton;
+        private System.Windows.Forms.Button deleteButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Bezier_curves
+{
+    public partial class Form1 : Form
+    {
+        public class BezierCurve
+        {
+            public List<PointF> Points { get; private set; }
+
+            public BezierCurve()
+            {
+                Points = new List<PointF>();
+            }
+
+            public void AddPoint(PointF point)
+            {
+                Points.Add(point);
+            }
+
+            public void MovePoint(int index, PointF newPosition)
+            {
+                if (index >= 0 && index < Points.Count)
+                {
+                    Points[index] = newPosition;
+                }
+            }
+        }
+
+        private List<BezierCurve> bezierCurves = new List<BezierCurve>();
+        private BezierCurve currentCurve = new BezierCurve();
+        private int selectedPointIndex = -1;
+        private bool addingMode = false;
+        private Color curveColor = Color.Blue;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            checkedListBox1.SelectedIndexChanged += checkedListBox1_SelectedIndexChanged;
+            checkedListBox1.ItemCheck += checkedListBox1_ItemCheck;
+            editButton.Click += editButton_Click;
+            deleteButton.Click += deleteButton_Click;
+        }
+
+        private void pictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (BezierCurve curve in bezierCurves)
+            {
+                if (curve.Points.Count == 4)
+                {
+                    e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                    Pen curvePen = new Pen(curveColor, 2);
+                    e.Graphics.DrawBezier(curvePen, curve.Points[0], curve.Points[1], curve.Points[2], curve.Points[3]);
+                    curvePen.Dispose();
+                }
+            }
+
+            if (currentCurve.Points.Count == 4)
+            {
+                e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                Pen curvePen = new Pen(Color.Blue, 2);
+                e.Graphics.DrawBezier(curvePen, currentCurve.Points[0], currentCurve.Points[1], currentCurve.Points[2], currentCurve.Points[3]);
+                curvePen.Dispose();
+            }
+
+            foreach (PointF point in currentCurve.Points)
+            {
+                e.Graphics.FillEllipse(Brushes.Blue, point.X - 3, point.Y - 3, 6, 6);
+            }
+        }
+
+        private void pictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            for (int i = 0; i < currentCurve.Points.Count; i++)
+            {
+                if (Math.Abs(currentCurve.Points[i].X - e.X) < 4 && Math.Abs(currentCurve.Points[i].Y - e.Y) < 4)
+                {
+                    selectedPointIndex = i;
+                    break;
+                }
+            }
+        }
+
+        private void pictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (selectedPointIndex != -1 && !addingMode)
+            {
+                currentCurve.MovePoint(selectedPointIndex, new PointF(e.X, e.Y));
+                pictureBox.Invalidate();
+            }
+        }
+
+        private void pictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (selectedPointIndex != -1 && e.Button == MouseButtons.Left && !addingMode)
+            {
+                selectedPointIndex = -1;
+            }
+        }
+
+        private bool IsPointUnderMouse(Point mouseLocation)
+        {
+            foreach (PointF point in currentCurve.Points)
+            {
+                if (Math.Abs(point.X - mouseLocation.X) < 4 && Math.Abs(point.Y - mouseLocation.Y) < 4)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void pictureBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && addingMode)
+            {
+                if (currentCurve.Points.Count == 4)
+                {
+                    addModeButton_Click(sender, e);
+                    return;
+                }
+                currentCurve.AddPoint(new PointF(e.X, e.Y));
+                pictureBox.Invalidate();
+            }
+            else if (e.Button == MouseButtons.Left && !addingMode)
+            {
+                return;
+            }
+
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            bezierCurves.Clear();
+            UpdateCheckedListBox();
+            currentCurve = new BezierCurve();
+            pictureBox.Invalidate();
+        }
+
+        private void AddBezierCurve(BezierCurve curve)
+        {
+            bezierCurves.Add(curve);
+            UpdateCheckedListBox();
+            currentCurve = new BezierCurve();
+            pictureBox.Invalidate();
+
+            curveColor = Color.Red;
+            selectedPointIndex = -1;
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedIndex = checkedListBox1.SelectedIndex;
+            if (selectedIndex >= 0 && selectedIndex < bezierCurves.Count)
+            {
+                currentCurve = bezierCurves[selectedIndex];
+                curveColor = Color.Blue;
+                pictureBox.Invalidate();
+            }
+        }
+
+        private void checkedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            if (addingMode)
+            {
+                e.NewValue = e.CurrentValue;
+            }
+        }
+
+        private void addModeButton_Click(object sender, EventArgs e)
+        {
+            addingMode = !addingMode;
+            if (!addingMode)
+            {
+                addModeButton.Text = "Add";
+                if (currentCurve.Points.Count > 0 && currentCurve.Points.Count < 4)
+                {
+                    AddBezierCurve(currentCurve);
+                }
+            }
+            else
+            {
+                addModeButton.Text = "Finish";
+                if (currentCurve.Points.Count == 4)
+                {
+                    AddBezierCurve(currentCurve);
+                }
+            }
+        }
+
+        private void editButton_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = checkedListBox1.SelectedIndex;
+            if (selectedIndex >= 0 && selectedIndex < bezierCurves.Count)
+            {
+                currentCurve = bezierCurves[selectedIndex];
+
+                addingMode = true;
+                addModeButton.Text = "Finish";
+
+                pictureBox.Invalidate();
+            }
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = checkedListBox1.SelectedIndex;
+            if (selectedIndex >= 0 && selectedIndex < bezierCurves.Count)
+            {
+                bezierCurves.RemoveAt(selectedIndex);
+                UpdateCheckedListBox();
+                pictureBox.Invalidate();
+            }
+        }
+
+        private void UpdateCheckedListBox()
+        {   
+            // Очищаем список
+            checkedListBox1.Items.Clear();
+
+            // Добавляем кривые в список
+            for (int i = 0; i < bezierCurves.Count; i++)
+            {
+                checkedListBox1.Items.Add($"Line {i + 1}");
+            }
+        }
+    }
+}
